7c8dd3b48455a211a0ed48b926001e1ad58254cd
Santhoshgande <127266737+santhoshgande@users.noreply.github.com>
2023-10-26 20:33:04 +0000

    Commiting test.py with string exxamples

--- Day-02/examples/test.py
+++ Day-02/examples/test.py
@@ -5,3 +5,37 @@ print(arn.upper())
 ll = len(arn)
 print("Length is", ll)
 
+#examples
+
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2
+print(result)
+
+text = "Python is awesome"
+length = len(text)
+print("Length of the string:", length)
+
+text = "Python is awesome"
+uppercase = text.upper()
+lowercase = text.lower()
+print("Uppercase:", uppercase)
+print("Lowercase:", lowercase)
+
+text = "Python is awesome"
+new_text = text.replace("awesome", "great")
+print("Modified text:", new_text)
+
+text = "Python is awesome"
+words = text.split()
+print("Words:", words)
+
+text = "   Some spaces around   "
+stripped_text = text.strip()
+print("Stripped text:", stripped_text)
+
+text = "Python is awesome"
+substring = "is"
+if substring in text:
+    print(substring, "found in the text")
+
