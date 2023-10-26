4e8d26df87aff483625b0f7394e227f3f952575d
Santhoshgande <127266737+santhoshgande@users.noreply.github.com>
2023-10-26 20:36:47 +0000

    Commiting with Integer and Float examples

--- Day-02/examples/test.py
+++ Day-02/examples/test.py
@@ -5,7 +5,7 @@ print(arn.upper())
 ll = len(arn)
 print("Length is", ll)
 
-#examples
+#String Examples
 
 str1 = "Hello"
 str2 = "World"
@@ -38,4 +38,44 @@ text = "Python is awesome"
 substring = "is"
 if substring in text:
     print(substring, "found in the text")
+
+#Number Examples
+
+# Float variables
+num1 = 5.0
+num2 = 2.5
+
+# Basic Arithmetic
+result1 = num1 + num2
+print("Addition:", result1)
+
+result2 = num1 - num2
+print("Subtraction:", result2)
+
+result3 = num1 * num2
+print("Multiplication:", result3)
+
+result4 = num1 / num2
+print("Division:", result4)
+
+# Rounding
+result5 = round(3.14159265359, 2)  # Rounds to 2 decimal places
+print("Rounded:", result5)
+
+# Integer variables
+num1 = 10
+num2 = 5
+
+# Integer Division
+result1 = num1 // num2
+print("Integer Division:", result1)
+
+# Modulus (Remainder)
+result2 = num1 % num2
+print("Modulus (Remainder):", result2)
+
+# Absolute Value
+result3 = abs(-7)
+print("Absolute Value:", result3)
+
 
